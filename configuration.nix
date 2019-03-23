74684a18cbab4d9a8853a40ac678e4f059a6df6f
Drew Smathers <drew.smathers@gmail.com>
2019-03-23 14:41:03 -0700

    enable static ip

--- configuration.nix
+++ configuration.nix
@@ -64,6 +64,7 @@
     lsof
     binutils
     lshw
+    iproute
 
     # Performance Testing
     sysbench
--- local-configuration##drewripper.nix
+++ local-configuration##drewripper.nix
@@ -51,13 +51,19 @@
     wireless = {
       enable = true;
     };
+    interfaces.wlp5s0.ipv4.addresses = [ {
+      address = "192.168.1.16";
+      prefixLength = 24;
+    } ];
+    defaultGateway = "192.168.1.1";
+    nameservers = [ "8.8.8.8" ];
   };
 
   hardware = {
     enableAllFirmware = true;
     pulseaudio = {
       enable = true;
-      package = pkgs.pulseaudioFull.override { jackaudioSupport = true; };
+      # package = pkgs.pulseaudioFull.override { jackaudioSupport = true; };
     };
     opengl = {
       driSupport32Bit = true;
