84fc97309c81d567d5e6f781a5952cdf8250124c
Drew Smathers <drew.smathers@oracle.com>
2020-02-26 22:04:53 -0800

    fix a comment setup.sh -> setup

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-# DO NOT FORGET TO RUN ./setup.sh FIRST
+# DO NOT FORGET TO RUN ./setup FIRST
 
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
